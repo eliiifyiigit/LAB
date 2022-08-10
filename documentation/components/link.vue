dc3ce9099a30f91b38e6246a3a0f002466f04be0
eliiifyiigit <eliiifyiig@gmail.com>
2022-08-10 21:29:58 +0300

    Pages

---
+++ documentation/components/link.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <el-row
+    justify="space-between"
+    :gutter="20"
+  >
+    <el-col
+      :md="{'offset':0, 'span':12}"
+      :lg="{'offset':0, 'span':12}"
+    >
+      <span style="font-size: 12px">WATSONS 2021</span><br>
+
+    </el-col>
+
+    <el-col
+      :md="{'offset':0, 'span':24}"
+      :lg="{'offset':0, 'span':24}"
+    >
+    </el-col>
+  </el-row>
+</div>
+</template>
+
+<script>
+export default {
+  name: "link"
+}
+</script>
+
+<style scoped>
+
+</style>
